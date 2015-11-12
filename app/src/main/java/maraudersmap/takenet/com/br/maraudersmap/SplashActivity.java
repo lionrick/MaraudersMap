424f1887e912440f68984bee662f84812ee60ec4
ricardos <ricardos@takenet.com.br>
2015-11-12 17:48:56 -0200

    Initial commit

---
+++ app/src/main/java/maraudersmap/takenet/com/br/maraudersmap/SplashActivity.java
@@ -0,0 +1,13 @@
+package maraudersmap.takenet.com.br.maraudersmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
