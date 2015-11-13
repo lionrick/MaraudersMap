e4450c6d502df75086d5229ac8982e2c3d5a001a
Ricardo dos Santos <ricardos@takenet.com.br>
2015-11-13 11:45:07 -0200

    animation to exibition

--- app/src/main/java/maraudersmap/takenet/com/br/maraudersmap/MainActivity.java
+++ app/src/main/java/maraudersmap/takenet/com/br/maraudersmap/MainActivity.java
@@ -1,7 +1,9 @@
 package maraudersmap.takenet.com.br.maraudersmap;
 
+import android.animation.Animator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import maraudersmap.takenet.com.br.maraudersmap.model.Pessoa;
 import maraudersmap.takenet.com.br.maraudersmap.model.PessoaDao;
@@ -39,5 +41,49 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+    }
+
+
+    private void animateView(final View view, int translationX, int translationY ) {
+        int distance = 100; //the distance to move in pixels
+        int duration = 1000; //the duration of the animation in ms
+
+        double direction = Math.random() * 2 * Math.PI;
+
+        translationX += (int) (Math.cos(direction) * distance);
+        translationY += (int) (Math.sin(direction) * distance);
+
+        if(translationX<0){
+            translationX = 0 ;
+        }
+
+        if(translationY<0){
+            translationY = 0;
+        }
+
+        final int finalTranslationY = translationY;
+        final int finalTranslationX = translationX;
+        view.animate().translationX(translationX).translationY(translationY).setDuration(duration).setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                System.out.println("x"+finalTranslationX+" y"+finalTranslationY);
+                animateView(view, finalTranslationX, finalTranslationY);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        }).start();
     }
 }
